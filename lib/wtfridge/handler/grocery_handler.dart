81cc400f7f17812de9dcd03d5acd1b7d83c30652
NathanRJohnson <robert.minecrafter.nathan@gmail.com>
2024-05-30 14:14:42 -0400

    adds most unit tests

--- lib/wtfridge/handler/grocery_handler.dart
+++ lib/wtfridge/handler/grocery_handler.dart
@@ -12,69 +12,65 @@ class GroceryHandler {
 
   GroceryHandler();
 
-  Future<void> pushToDB(GroceryItem i) async {
+  Future<void> pushToDB(Client client, GroceryItem i) async {
     var body = jsonEncode(i);
 
-    var response = await post(url, body: body);
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      print("Item added!");
-    } else {
-      print("Failed to add Item: [${response.statusCode}] ${response.body}");
+    var response = await client.post(url, body: body);
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw ClientException("Failed to add Item: [${response.statusCode}] ${response.body}");
     }
   }
 
-  Future<List<GroceryItem>> getAllItems() async {
+  // TODO: fix api to not return the word null on empty list
+  Future<List<GroceryItem>> getAllItems(Client client) async {
     List<GroceryItem> dbItems = [];
 
-    var response = await get(url);
-    if (response.statusCode == 200) {
-      // TODO: fix api to not return the word null on empty list
-      if (response.body != "null" && response.body.isNotEmpty) {
-        List<dynamic> jsonItems = json.decode(response.body);
-        for (Map<String, dynamic> itemMap in jsonItems) {
-          try {
-            GroceryItem newItem = GroceryItem.fromJSON(itemMap);
-            dbItems.add(newItem);
-          } on Exception catch (e) {
-            print(e.toString());
-            continue;
-          }
+    var response = await client.get(url);
+
+    if (response.statusCode != 200) {
+      throw ClientException("Unable to access items from db: [${response.statusCode}]");
+    }
+
+    else if (response.body.isNotEmpty && response.body != "null") {
+      List<dynamic> jsonItems = json.decode(response.body);
+      for (Map<String, dynamic> itemMap in jsonItems) {
+        try {
+          GroceryItem newItem = GroceryItem.fromJSON(itemMap);
+          dbItems.add(newItem);
+        } on Exception catch (e) {
+          print(e.toString());
+          continue;
         }
       }
     }
+
     return dbItems;
   }
 
-  Future<void> deleteItemByID(int itemID) async {
+  Future<void> deleteItemByID(Client client, int itemID) async {
     var request = url.resolve(itemID.toString());
 
-    var response = await delete(request);
-    if (response.statusCode == 200) {
-      print("Grocery item deleted!");
-    } else {
-      print("Failed to delete grocery item");
+    var response = await client.delete(request);
+    if (response.statusCode != 200) {
+      throw ClientException("Failed to delete grocery item");
     }
   }
 
-  Future<void> toggleActiveByID(int itemID) async {
+  Future<void> toggleActiveByID(Client client, int itemID) async {
     var request = url.resolve(itemID.toString());
 
-    var response = await patch(request);
-    if (response.statusCode == 200) {
-      print("Grocery item toggled active!");
-    } else {
-      print("Failed to toggle grocery item");
+    var response = await client.patch(request);
+    if (response.statusCode != 200) {
+      throw ClientException("Failed to toggle grocery item");
     }
   }
 
-  Future<void> sendActiveToFridge() async {
+  Future<void> sendActiveToFridge(Client client) async {
     var request = url.resolve("to_fridge");
 
-    var response = await post(request);
+    var response = await client.post(request);
     if (response.statusCode == 200) {
-      print("Active grocery items sent to fridge!");
-    } else {
-      print("Failed send active grocery items to fridge");
+      throw ClientException("Failed send active grocery items to fridge");
     }
   }
 
---
+++ test/grocery_handler_test.dart
@@ -0,0 +1,169 @@
+
+
+import 'dart:convert';
+
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../lib/wtfridge/handler/grocery_handler.dart';
+import '../lib/wtfridge/model/grocery_item.dart';
+import 'fridge_handler_test.mocks.dart';
+
+// dart run build_runner build
+@GenerateMocks([Client])
+void main() {
+  late Uri url;
+  late GroceryHandler handler;
+  late String body;
+
+  setUp(() {
+    url = Uri.http("3.96.14.111", "/grocery/");
+    handler = GroceryHandler();
+  });
+
+  group('get all items', () {
+    body = '[{"item_id": 123, "item_name":"G", "is_active": false}]';
+    test(' returns a GroceryItem if http call completes successfully', () async {
+      final client = MockClient();
+
+      when(client
+          .get(url))
+            .thenAnswer((_) async =>
+                Response(body, 200));
+
+      expect(await handler.getAllItems(client), isA<List<GroceryItem>>());
+    });
+
+    test('returns empty list if the http returns with empty body', () async {
+      final client = MockClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(client
+          .get(url))
+          .thenAnswer((_) async =>
+          Response('', 200));
+
+      expect(await handler.getAllItems(client), <GroceryItem>[]);
+    });
+
+    test('raises error if the http call completes with an error', () async {
+      final client = MockClient();
+
+      when(client
+          .get(url))
+          .thenAnswer((_) async =>
+          Response('Server Error', 500));
+
+      expect(handler.getAllItems(client), throwsA(const TypeMatcher<ClientException>()));
+    });
+  });
+
+  group('push to db', () {
+    GroceryItem item = GroceryItem(id: 123, name: "G", isActive: false);
+
+    test('successful item addition', () async {
+      final client = MockClient();
+
+      when(client
+          .post(url, body: jsonEncode(item)))
+          .thenAnswer((_) async =>
+          Response('', 200));
+
+      await expectLater(handler.pushToDB(client, item), completes);
+    });
+
+    test('failed item addition throws ClientException', () async {
+      final client = MockClient();
+
+      when(client
+          .post(url, body: jsonEncode(item)))
+          .thenAnswer((_) async =>
+          Response('', 500));
+
+      expect(handler.pushToDB(client, item), throwsA(const TypeMatcher<ClientException>()));
+    });
+  });
+
+  group('delete from db', () {
+    GroceryItem item = GroceryItem(id: 123, name: "G", isActive: false);
+
+    test('successful item deletion', () async {
+      final client = MockClient();
+
+      when(client
+          .delete(url.resolve(item.id!.toString())))
+          .thenAnswer((_) async =>
+          Response('', 200));
+
+      expect(handler.deleteItemByID(client, item.id!), completes);
+    });
+
+    test('failed item deletion throws ClientException', () async {
+      final client = MockClient();
+
+      when(client
+          .delete(url.resolve(item.id!.toString())))
+          .thenAnswer((_) async =>
+          Response('', 500));
+
+      expect(handler.deleteItemByID(client, item.id!), throwsA(const TypeMatcher<ClientException>()));
+    });
+  });
+
+  group('toggle item active state', () {
+    GroceryItem item = GroceryItem(id: 123, name: "G", isActive: false);
+
+    test('successful item deletion', () async {
+      final client = MockClient();
+
+      when(client
+          .patch(url.resolve(item.id!.toString())))
+          .thenAnswer((_) async =>
+          Response('', 200));
+
+      expect(handler.toggleActiveByID(client, item.id!), completes);
+    });
+
+    test('failed item deletion throws ClientException', () async {
+      final client = MockClient();
+
+      when(client
+          .patch(url.resolve(item.id!.toString())))
+          .thenAnswer((_) async =>
+          Response('', 500));
+
+      expect(handler.toggleActiveByID(client, item.id!), throwsA(const TypeMatcher<ClientException>()));
+    });
+  });
+
+  group('toggle item active state', () {
+    GroceryItem item = GroceryItem(id: 123, name: "G", isActive: false);
+
+    test('successful item deletion', () async {
+      final client = MockClient();
+
+      when(client
+          .patch(url.resolve(item.id!.toString())))
+          .thenAnswer((_) async =>
+          Response('', 200));
+
+      expect(handler.toggleActiveByID(client, item.id!), completes);
+    });
+
+    test('failed item deletion throws ClientException', () async {
+      final client = MockClient();
+
+      when(client
+          .patch(url.resolve(item.id!.toString())))
+          .thenAnswer((_) async =>
+          Response('', 500));
+
+      expect(handler.toggleActiveByID(client, item.id!), throwsA(const TypeMatcher<ClientException>()));
+    });
+  });
+}
