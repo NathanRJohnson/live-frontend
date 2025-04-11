bf9984b24aad6b29df4b097d8d4719290c6d3f15
NathanRJohnson <robert.minecrafter.nathan@gmail.com>
2025-04-11 12:13:36 -0400

    settings drawer

--- lib/wtfridge/components/settings_drawer.dart
+++ lib/wtfridge/components/settings_drawer.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_l/wtfridge/components/login_button.dart';
 
-class SettingsPanel extends StatelessWidget {
-  const SettingsPanel({super.key});
+class SettingsDrawer extends StatelessWidget {
+  const SettingsDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
